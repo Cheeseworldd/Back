977c3ada8052d2cf67fb87420d069c79a374a065
Cheeseworldd <87156906+Cheeseworldd@users.noreply.github.com>
2024-05-31 22:12:55 +1000

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,10 +1,13 @@
-const allowedCors = [ "http://frront.nomorepartiesco.ru/" ];
+const allowedCors = [ 
+  "http://frront.nomorepartiesco.ru/",
+  "https://baack.nomorepartiesco.ru/"
+ ];
 
 const cors = (request, response, next) => {
     const { origin } = request.headers;
   
     if (allowedCors.includes(origin)) {
-      response.header('Access-Control-Allow-Origin', origin);
+      response.header('Access-Control-Allow-Origin', "*");
       response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
       response.header(
         'Access-Control-Allow-Headers',

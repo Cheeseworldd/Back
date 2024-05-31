2ec4c60489749462ef0261344517a76ca567c541
Cheeseworldd <87156906+Cheeseworldd@users.noreply.github.com>
2024-05-31 21:46:32 +1000

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,23 +1,21 @@
-// Вместо нашего адреса вставь свой
-// Можно ещё не удалять адреса с localhost, но мы удалили для краткости
 const allowedCors = [
-  "http://frront.nomorepartiesco.ru/",
-  "http://localhost:3001",
-  "http://localhost:3000",
+    'https://practicum.yandex.ru',
+    "http://frront.nomorepartiesco.ru/",
+    'https://students-projects.ru',
+    'http://localhost:3000'
 ];
 
 function cors(req, res, next) {
-  const { origin } = req.headers;
-
-  if (allowedCors.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
-  );
-  next();
+    const { origin } = req.headers;
+    if (allowedCors.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+    }
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+    next()
 }
 
-module.exports = cors;
+module.exports = {
+    cors
+}   
+
